52232eb26acceb3ae4a981ea7ea1e475f9b252a8
realdubb <aleqs@gmx.com>
2016-12-07 19:29:57 -0600

    Refactored contact controller to be simpler like in example

--- app/controllers/contact.js
+++ app/controllers/contact.js
@@ -3,16 +3,11 @@ import Ember from 'ember';
 export default Ember.Controller.extend({
 
   emailAddress: '',
-  message: '',
-
-  messageLength: Ember.computed('message', function(){
-    return this.get('message').length;
-  }),
 
   emailValid:   Ember.computed.match('emailAddress', /^.+@.+\..+$/),
   allValid:     Ember.computed.and('emailValid', 'messageValid'),
   isDisabled:   Ember.computed.not('allValid'),
-  messageValid: Ember.computed.gte('messageLength', 5),
+  messageValid: Ember.computed.gte('message.length', 5),
 
   actions:{
 
